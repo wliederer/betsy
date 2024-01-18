b5fbcf20410920dee95be1f162e4f549288e5115
william liederer <will.liederer@playpickup.com>
2024-01-18 08:44:39 -0500

    getting ready for the big day

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("lets go!");
+
+fetch(
+  `https://api-staging.playpickup.com/v2/contests/10/leaderboard?token=f39befcb-1b91-4ec1-95d7-9d00db67b6d9`
+)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json(); // Parse the response body as JSON
+  })
+  .then((data) => {
+    // console.log(data);
+    const leaderboard = document.getElementById("leaderboard");
+    data.forEach((d) => {
+      const li = document.createElement("li");
+      li.textContent = d.username + " - " + d.total;
+      leaderboard.appendChild(li);
+    });
+  })
+  .catch((error) => {
+    console.error("Fetch error:", error);
+  });
