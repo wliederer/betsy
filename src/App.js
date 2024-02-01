c1fee735a2af060fd95392a0578139cbf3548bab
william liederer <will.liederer@playpickup.com>
2024-02-01 13:21:31 -0500

    add rolly

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import Header from './Components/Header'
 import Overlay from './Components/Overlay'
 import ProductGrid from './Components/ProductGrid'
 import React, { useState } from 'react'
-import SendEmail from './Components/SendEmail'
+import SendEmail, { handleSendEmail } from './Components/SendEmail'
+import { Rolly } from 'rolly-polly-polls'
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
+
+  const onLoad = (data) => {
+    console.log(data)
+  }
+  const onPick = async (data) => {
+    console.log('Pick Made', data)
+    let message = data.title + ' ' + data.pick
+    handleSendEmail(message)
+  }
+
   return (
     <div className="App">
       <Header open={isOpen} setIsOpen={setIsOpen} />
       {isOpen ? <Overlay /> : null}
+      <div className="rolly">
+        <Rolly
+          title={'How much are you willing to spend on a sticker?'}
+          picks={['1$', '2$', '3$', '4$', '5$', 'more!']}
+          pickMessage={'Thankyou for your pick! We have received your feedback'}
+          onLoad={onLoad}
+          onPick={onPick}
+          theme={'betsy'}
+        />
+      </div>
       <ProductGrid />
       <SendEmail />
     </div>
--- src/Components/SendEmail.js
+++ src/Components/SendEmail.js
@@ -5,26 +5,21 @@ import emailjs from '@emailjs/browser'
 const SERVICE_ID = 'service_8om0mif'
 const TEMPLATE_ID = 'template_spprxgg'
 const PUBLIC_KEY = '2eyHiJYODn1OC-921'
+export const handleSendEmail = async (message) => {
+  // if (e) e.preventDefault()
+
+  emailjs.send(SERVICE_ID, TEMPLATE_ID, { message: message }, PUBLIC_KEY).then(
+    (result) => {
+      console.log(result.text)
+    },
+    (error) => {
+      console.log(error.text)
+    },
+  )
+}
 
 const SendEmail = () => {
   const [message, setMessage] = useState('')
-  const form = useRef()
-
-  const handleSendEmail = (e) => {
-    e.preventDefault()
-
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
-      (result) => {
-        console.log(result.text)
-      },
-      (error) => {
-        console.log(error.text)
-      },
-    )
-
-    // Clear the input fields after sending the email
-    setMessage('')
-  }
 
   return (
     <div className="email-container">
@@ -33,7 +28,7 @@ const SendEmail = () => {
       </div>
 
       <div>
-        <form ref={form} onSubmit={handleSendEmail}>
+        <form>
           <textarea
             id="message"
             name="message"
@@ -41,7 +36,13 @@ const SendEmail = () => {
             onChange={(e) => setMessage(e.target.value)}
           />
           <div>
-            <button className="send" type="submit">
+            <button
+              className="send"
+              onClick={(e) => {
+                e.preventDefault()
+                handleSendEmail(message).then(() => setMessage(''))
+              }}
+            >
               Send Email
             </button>
           </div>
