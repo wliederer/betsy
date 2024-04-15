fc0c994e6546cc7675f9eaf4bcfd6e51d5b9266d
william liederer <will.liederer@playpickup.com>
2024-04-15 09:02:28 -0400

    custom font

--- src/Components/FAQSection.js
+++ src/Components/FAQSection.js
@@ -8,7 +8,7 @@ const FAQSection = () => {
             <div className="faq-list">
                 <div className="faq-item">
                     <h3 className="faq-question">How do I receive free stickers?</h3>
-                    <p className="faq-answer">After submitting an address stickers will be mailed in an envelope!</p>
+                    <p className="faq-answer">After submitting an address a random selection of stickers will be mailed in an envelope!</p>
                 </div>
                 <div className="faq-item">
                     <h3 className="faq-question">How long will it take to receive my stickers?</h3>
@@ -16,7 +16,11 @@ const FAQSection = () => {
                 </div>
                 <div className="faq-item">
                     <h3 className="faq-question">Is there a limit to how many stickers I can request?</h3>
-                    <p className="faq-answer">No, there's no limit! Feel free to request as many stickers as you want.  Stickers will typically be sent out once a month.</p>
+                    <p className="faq-answer">No, there's no limit! Feel free to request as often as you want.  Stickers will typically be sent out once a month.  Right now I am just monitoring my inbox and mailing stickers as I see requests.  If this operation ever grows I will implement a more structured approach.</p>
+                </div>
+                <div className="faq-item">
+                    <h3 className="faq-question">What is the quality of stickers?</h3>
+                    <p className="faq-answer">Stickers are inkjet printed and laminated by me.  The laminate will make them resistant to most situations including the dishwasher!</p>
                 </div>
                 {/* Add more FAQ items as needed */}
             </div>
