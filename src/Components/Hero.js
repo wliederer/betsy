25ec963c2b3ca633f48ec8b7aa06330f12001767
william liederer <will.liederer@playpickup.com>
2024-06-08 12:53:34 -0400

    updating hero

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className="hero-container">
             <div className="hero-content">
-                {/* <h1 className="hero-title">Get Free Stickers!</h1> */}
+                <h1 className="hero-title">Oh Shit</h1>
                 {/* <h2 className="hero-subtitle">Brighten Your Day with our Free Sticker Collection</h2> */}
                 {/* <p className="hero-description">Submit an address to receive this months free stickers!</p> */}
                 {/* <button className="hero-btn">Get Started</button> */}
